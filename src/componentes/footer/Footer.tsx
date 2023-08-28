f35ad036e6af354a4f5b849175fd8d10fe9bdd8c
thais0 <thais.santos13@estudante.ifms.edu.br>
2023-08-28 12:06:14 -0400

    atividade 28/08

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,28 @@
+import './Footer.css'
+function Footer(){
+    return(
+        <footer>
+            <div>
+        <h1 className="lg">Thais</h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Sobre</a></li>
+        </ul>
+        <ul>
+         
+            <li><a href="#">Politíca de Privacidade</a></li>
+            <li><a href="#">Termos e Condições</a></li>
+        </ul>
+        <ul>
+ 
+            <li><a href="#">Email: thais.santos@gmail.com</a></li>
+            <li><a href="#">Fone: 99999-9999</a></li>
+        </ul>
+    </nav>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,27 @@
+import './Header.css'
+function Header(){
+    return(
+        <header>
+    <div>
+        <h1 className="lg">Thais</h1>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Projetos</a>
+            </li>
+            <li>
+                <a href="#">Contatos</a>
+            </li>
+            <li>
+                <a href="#">Sobre</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+    )
+}
+export default Header    
